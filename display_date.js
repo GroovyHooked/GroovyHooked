4e61ca7335b3e37cf109996367de1221888593ec
groovyhooked <dafrenchie2002@yahoo.fr>
2025-02-04 11:26:17 +0100

    Improving cron task

--- display_date.js
+++ display_date.js
@@ -5,11 +5,18 @@ const exec = require('child_process').execSync;
 const README_PATH = path.join(__dirname, './README.md');
 const README_CONTENT = fs.readFileSync(README_PATH, 'utf8');
 
+// Generate a random number of zero-width spaces
+const getRandomInvisibleString = () => {
+  const zeroWidthSpace = '\u200B';
+  const count = Math.floor(Math.random() * 5) + 1;
+  return zeroWidthSpace.repeat(count);
+};
+
 const today = new Date().toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
 
 const newContent = README_CONTENT.replace(
   /<!-- display date -->(.*?)<!-- display date end -->/s,
-  `<!-- display date -->\n${today}\n<!-- display date end -->`
+  `<!-- display date -->\n${today}${getRandomInvisibleString()}\n<!-- display date end -->`
 );
 
 fs.writeFileSync(README_PATH, newContent);
